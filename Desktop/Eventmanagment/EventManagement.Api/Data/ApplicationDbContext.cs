4f1a6049544d617852437240db767822e9042701
osvat0rres <osvaldotorres3108@gmail.com>
2026-02-27 18:52:56 -0800

    Replace folder contents

---
+++ Desktop/Eventmanagment/EventManagement.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using EventManagement.Api.Models;
+
+namespace EventManagement.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Event> Events => Set<Event>();
+    }
+}
